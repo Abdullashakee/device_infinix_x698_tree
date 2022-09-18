c332eb3578f9362eee4f6c5144c754bb5a4a7b7d
SamarV-121 <samarvispute121@pm.me>
2022-09-18 12:03:05 +0100

    fleur: libshim_vtservice: Update to S [1] LineageOS/android_frameworks_av@bda4575#diff-0e7ff81a94e7f5ce90bdb128b6ceea84c201a721055b05afccfa2ec814036907 [2] LineageOS/android_frameworks_av@3376113#diff-0e7ff81a94e7f5ce90bdb128b6ceea84c201a721055b05afccfa2ec814036907

--- libshims/libshim_vtservice.cpp
+++ libshims/libshim_vtservice.cpp
@@ -4,7 +4,8 @@
 using namespace android;
 
 extern "C" {
-void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_o>        audio_stream_type_t streamType,
+void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi(
+        audio_stream_type_t streamType,
         uint32_t sampleRate,
         audio_format_t format,
         audio_channel_mask_t channelMask,
