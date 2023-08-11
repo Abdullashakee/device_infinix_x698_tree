479d4d872c34f00ea0d0bf1d5e6b24b693b33936
ZunayedDihan <zunayeddihan@gmail.com>
2023-08-11 06:17:24 +0600

    x6827: drop keylayout

--- device.mk
+++ device.mk
@@ -170,11 +170,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ImsInit
 
-# Keylayout
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
-    $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl \
-
 # Auto-Brightness
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/android.hardware.lights-service.mediatek:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.lights-service.mediatek \
